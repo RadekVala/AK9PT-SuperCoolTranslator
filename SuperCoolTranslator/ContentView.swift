6062b2a6cf1369e6597d435e725d4a70829ecc27
Radek Vala <radek.www@gmail.com>
2023-12-01 18:39:19 +0100

    Complete app

--- SuperCoolTranslator/ContentView.swift
+++ SuperCoolTranslator/ContentView.swift
@@ -8,17 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var userInput = ""
+    @State var translationResult = ""
+    
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
+                .resizable()
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+                .frame(width: 150,height: 150)
+            
+            TextField("Enter text for translation...", text: $userInput )
+                .padding()
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            Button("Translate", action: doRequest)
+                .padding()
+            
+            Text("\(translationResult)")
         }
         .padding()
     }
+    
+    func doRequest(){
+        // api call
+        print("btn tapped")
+        
+        if !userInput.isEmpty {
+            
+            let originalUrl = "https://api.mymemory.translated.net/get?q=\(userInput)&langpair=cs|en"
+            let escapedUrl = originalUrl.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+            
+            guard let url = URL(string: escapedUrl!) else {
+                print("Invalid URL")
+                return
+            }
+            
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                if let data = data {
+                    do {
+                        let decoder = JSONDecoder()
+                        let result = try decoder.decode(TranslationResponse.self, from: data)
+                        DispatchQueue.main.async {
+                            translationResult = result.responseData.translatedText
+                        }
+                    } catch {
+                        print("Error decoding JSON: \(error)")
+                    }
+                } else if let error = error {
+                    print("Error fetching data: \(error)")
+                }
+            }.resume()
+        }
+    }
 }
 
-#Preview {
-    ContentView()
-}
+/*
+ #Preview {
+ ContentView()
+ }
+ */
--- SuperCoolTranslator/model/TranslationResponse.swift
+++ SuperCoolTranslator/model/TranslationResponse.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct TranslationResponse: Codable {
+    let responseData: ResponseData
+   
+}
+
+struct ResponseData: Codable {
+    let translatedText: String
+    let match: Double
+}
+
