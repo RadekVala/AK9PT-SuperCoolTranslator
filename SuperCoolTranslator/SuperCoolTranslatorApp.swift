4abd9d07099a72c42cbd7f46b9e045fc47aa8e80
Radek Vala <radek.www@gmail.com>
2023-12-01 17:45:18 +0100

    Initial Commit

---
+++ SuperCoolTranslator/SuperCoolTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperCoolTranslatorApp.swift
+//  SuperCoolTranslator
+//
+//  Created by Radek Vala on 01.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SuperCoolTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
